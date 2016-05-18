280e3bdd21b6538d3b4c08244f4e6da6f58b5917
Jacob Thomas <jacob6841@008c1mmkqjkdty3.ncsd.k12.wy.us>
2016-05-18 15:06:18 -0600

    Initial Commit
    Heathcliff Structure commit

---
+++ Arcania/Code/Testing Environments/Heathcliff/desktop/src/com/arcanereality/heathcliff/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.arcanereality.heathcliff.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.arcanereality.heathcliff.Arcania;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Arcania(), config);
+	}
+}
